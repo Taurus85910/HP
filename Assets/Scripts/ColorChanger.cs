498baa0788de2241607a64d5dd985e2d17f9603b
Kostya <96919923@mail.ru>
2020-12-30 14:19:17 +1000

    1

---
+++ Assets/Scripts/ColorChanger.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Image))]
+public class ColorChanger : MonoBehaviour
+{
+    private Image _image;
+    [SerializeField] private Slider _slider;
+    private void Start()
+    {
+        _image = GetComponent<Image>();
+    }
+
+    public void Update()
+    {
+         _image.color = Color.Lerp(Color.red, Color.green, _slider.value / _slider.maxValue);       
+    }
+}
---
+++ Assets/Scripts/ValueChanger.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using DG.Tweening;
+
+public class ValueChanger : MonoBehaviour
+{
+    private Slider _slider;
+    [SerializeField] private float _changeTime;
+
+    private void Start()
+    {
+        _slider = GetComponent<Slider>();
+    }
+
+
+    public void ChangeValue(float Value) => _slider.DOValue(_slider.value + Value, _changeTime);
+}
